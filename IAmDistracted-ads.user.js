fd725461c0c0396ec303327b387bf162c05ba3de
bansan85 <legarrec.vincent@gmail.com>
2017-01-07 15:42:58 +0100

    Add different filter for mailing list and adblock.

---
+++ IAmDistracted-ads.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name        I Am Distracted
+// @namespace   https://github.com/bansan85/IAmDistracted
+// @description Help to keep you focus while browsing. Disabling ads.
+// @include     http://*
+// @include     https://*
+// @license     https://opensource.org/licenses/MIT
+// @homepage    https://github.com/bansan85/IAmDistracted
+// @updateURL   https://raw.githubusercontent.com/bansan85/IAmDistracted/master/IAmDistracted-ads.meta.js
+// @downloadURL https://raw.githubusercontent.com/bansan85/IAmDistracted/master/IAmDistracted-ads.user.js
+// @version     0.1
+// @grant       none
+// @run-at      document-end
+// ==/UserScript==
--- IAmDistracted-mailing.user.js
+++ IAmDistracted-mailing.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name        I Am Distracted
 // @namespace   https://github.com/bansan85/IAmDistracted
-// @description Help to keep you focus while browsing.
+// @description Help to keep you focus while browsing. Disabling mailing list.
 // @include     http://*
 // @include     https://*
 // @license     https://opensource.org/licenses/MIT
 // @homepage    https://github.com/bansan85/IAmDistracted
-// @updateURL   https://raw.githubusercontent.com/bansan85/IAmDistracted/master/IAmDistracted.meta.js
-// @downloadURL https://raw.githubusercontent.com/bansan85/IAmDistracted/master/IAmDistracted.user.js
+// @updateURL   https://raw.githubusercontent.com/bansan85/IAmDistracted/master/IAmDistracted-mailing.meta.js
+// @downloadURL https://raw.githubusercontent.com/bansan85/IAmDistracted/master/IAmDistracted-mailing.user.js
 // @version     0.1
 // @grant       none
 // @run-at      document-end
