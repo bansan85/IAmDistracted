c622bb800e2d82dbf54bfeab81c1a2785022aed4
bansan85 <legarrec.vincent@gmail.com>
2017-01-20 15:33:14 +0100

    Add unrelated-services with lemonde.fr.

--- IAmDistracted-unrelated-services.user.js
+++ IAmDistracted-unrelated-services.user.js
@@ -8,13 +8,28 @@
 // @homepage    https://github.com/bansan85/IAmDistracted
 // @updateURL   https://raw.githubusercontent.com/bansan85/IAmDistracted/master/IAmDistracted-unrelated-services.meta.js
 // @downloadURL https://raw.githubusercontent.com/bansan85/IAmDistracted/master/IAmDistracted-unrelated-services.user.js
-// @version     0.1
+// @version     0.1.1
 // @grant       none
 // @run-at      document-end
 // ==/UserScript==
+
+function getElementsByClassName(classname)
+{
+  var a = [];
+  var re = new RegExp('\\b' + classname + '\\b');
+  var els = document.all ? document.all : document.getElementsByTagName("*");
+  for(var i=0,j=els.length; i<j; i++)
+    if(re.test(els[i].className))a.push(els[i]);
+  return a;
+}
 
 if (window.location.href.match(/http[s]?:\/\/www.lemonde.fr/)) {
   // Test 0010_lemonde_fr_4
   var elmDeleted = document.getElementById('bandeau_bizdev');
   elmDeleted.parentNode.removeChild(elmDeleted);
+  // Test 0011_lemonde_fr_5
+  var elmDeletedList = getElementsByClassName("promo");
+  for (var i=elmDeletedList.length - 1; i >= 0; i--) {
+    elmDeletedList[i].parentNode.removeChild(elmDeletedList[i]);
+  }
 }
