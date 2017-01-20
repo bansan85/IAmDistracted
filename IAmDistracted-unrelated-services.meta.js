a70474fd60b9a94847c7199d859cf27892944205
bansan85 <legarrec.vincent@gmail.com>
2017-01-20 12:18:43 +0100

    Add Unrelated Services to GreaseMonkey.

---
+++ IAmDistracted-unrelated-services.meta.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name        I Am Distracted - Mailing
+// @namespace   https://github.com/bansan85/IAmDistracted.unrelated-services
+// @description Help to keep you focus while browsing. Disabling unrelated services frame.
+// @include     http://*
+// @include     https://*
+// @license     https://opensource.org/licenses/MIT
+// @homepage    https://github.com/bansan85/IAmDistracted
+// @updateURL   https://raw.githubusercontent.com/bansan85/IAmDistracted/master/IAmDistracted-unrelated-services.meta.js
+// @downloadURL https://raw.githubusercontent.com/bansan85/IAmDistracted/master/IAmDistracted-unrelated-services.user.js
+// @version     0.1
+// @grant       none
+// @run-at      document-end
+// ==/UserScript==
