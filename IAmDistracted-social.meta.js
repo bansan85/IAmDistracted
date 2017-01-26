78bf52c34be9e0a8b832210a755c4819f28a6c18
bansan85 <legarrec.vincent@gmail.com>
2017-01-26 09:15:16 +0100

    Add social network.

---
+++ IAmDistracted-social.meta.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name        I Am Distracted - Social network
+// @namespace   https://github.com/bansan85/IAmDistracted.social
+// @description Help to keep you focus while browsing. Disabling social network.
+// @include     http://*
+// @include     https://*
+// @license     https://opensource.org/licenses/MIT
+// @homepage    https://github.com/bansan85/IAmDistracted
+// @updateURL   https://raw.githubusercontent.com/bansan85/IAmDistracted/master/IAmDistracted-social.meta.js
+// @downloadURL https://raw.githubusercontent.com/bansan85/IAmDistracted/master/IAmDistracted-social.user.js
+// @version     0.1
+// @grant       none
+// @run-at      document-end
+// ==/UserScript==
