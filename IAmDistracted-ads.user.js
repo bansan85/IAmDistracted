444ce9d38b4eefc1bd0eea0896ccc0e3dda46743
bansan85 <legarrec.vincent@gmail.com>
2017-07-14 10:43:26 +0200

    Remove Facebook Ads.

--- IAmDistracted-ads.user.js
+++ IAmDistracted-ads.user.js
@@ -8,14 +8,35 @@
 // @homepage    https://github.com/bansan85/IAmDistracted
 // @updateURL   https://raw.githubusercontent.com/bansan85/IAmDistracted/master/IAmDistracted-ads.meta.js
 // @downloadURL https://raw.githubusercontent.com/bansan85/IAmDistracted/master/IAmDistracted-ads.user.js
-// @version     0.1.1
+// @version     0.1.2
 // @grant       none
 // @run-at      document-end
 // ==/UserScript==
+
+function getElementsByClassName(classname)
+{
+  var a = [];
+  var re = new RegExp('\\b' + classname + '\\b');
+  var els = document.all ? document.all : document.getElementsByTagName("*");
+  for(var i=0,j=els.length; i<j; i++)
+    if(re.test(els[i].className))a.push(els[i]);
+  return a;
+}
 
 if (window.location.href.match(/http[s]?:\/\/www.facebook.com/)) {
   // Test 0009_facebook_com_2
   var elmDeleted = document.getElementById("substream_1");
   elmDeleted.parentNode.removeChild(elmDeleted);
+  var target = document.getElementById("stream_pagelet");
+  var observer = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+      var elmDeletedList = getElementsByClassName("_5g-l");
+      for (var i=elmDeletedList.length - 1; i >= 0; i--) {
+        elmDeletedList[i].parentNode.parentNode.parentNode.parentNode.removeChild(elmDeletedList[i].parentNode.parentNode.parentNode);
+      }
+    });
+  });
+  var config = { attributes: true, childList: true, characterData: true, subtree: true };
+  observer.observe(target, config);
 }
 
